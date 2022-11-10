ca50c1b47968a96c239c1a126b7ae66915fd8f89
sadikbasha-00 <bashasadik993@gmail.com>
2022-11-10 12:03:38 +0530

    added

---
+++ Travel_project/Travel_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here
+from django.shortcuts import render
+from .models import Place, Team
+
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj2=Team.objects.all()
+    return render(request,'index.html',{'result':obj,'result2':obj2})
